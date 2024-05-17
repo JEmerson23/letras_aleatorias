918e62b8df8eae69d3caee92a41b938d12e820e6
JEmerson23 <emersonbezerra174@gmail.com>
2024-05-17 15:27:35 -0300

    CMT-19: change melhorias na formatacao do index.js e mudanca do loop while para o do...while; add comentarios em index.js

--- script/index.js
+++ script/index.js
@@ -10,24 +10,39 @@ window.addEventListener(
         document.getElementById("input_first_letter"),
       $inputLastLetter = document.getElementById("input_last_letter");
 
+      
+
       $generateBtn.addEventListener(
-        "click", 
+        "click", $inputFirstLetter && $inputLastLetter ? 
         () => {
           $output.innerText = 
-            generate($inputFirstLetter.value[0], $inputLastLetter.value[0]);
-        }
+            generate(
+              $inputFirstLetter.value[0], 
+              $inputLastLetter.value[0]
+            );
+        } :
+        () => {$output.innerHTML = generate()}
+      );
+
+    } else {
+      console.error(
+        "não encontrado elemento com id '$generate_button' \
+        e/ou elemento com id '$output_display'..."
       );
     }
   }
 );
 
 function generate(from, to) {
+  // gerar letra aleatória
   const letters = ((alphabet) => {
     const i1 = alphabet.indexOf(from),
     i2 = alphabet.indexOf(to);
 
     if(i1 != -1 && i2 != -1) {
-      return i1 - i2 < 0 ? alphabet.slice(i1, i2+1) : alphabet.slice(i2, i1+1);
+      return 
+        i1 - i2 < 0 ?
+          alphabet.slice(i1, i2+1) : alphabet.slice(i2, i1+1);
     } 
 
     return alphabet;
@@ -35,8 +50,8 @@ function generate(from, to) {
   LETTERS_LEN = letters.length;
 
   let index, letter, BREAKER = 0;
-
-  while(typeof letter !== "string") {
+  
+  do {
     index = parseInt(Math.random()*LETTERS_LEN);
     letter = letters[index];
 
@@ -50,7 +65,7 @@ function generate(from, to) {
     if(BREAKER > 999) break;
 
     BREAKER++;
-  }
+  } while(typeof letter !== "string");
 
   return letter;
 }
